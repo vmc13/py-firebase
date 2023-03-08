897c919416b283aefb8d9b4550a2a4de74215bd1
Victória Mendes <vick.costa1305@gmail.com>
2023-03-08 10:57:13 -0300

    commit

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ dic_req = req.json()
 # adicionar as chaves numa lista
 list_keys = []
 
+# adiciona as keys do json de imagens a list_keys
 for items in dic_req.keys():
     #print(items)
     list_keys.append(items)
@@ -36,8 +37,8 @@ for item_id in dic_req:
     status = dic_req[item_id]['status']
     #print(item_id)
     #print(dic_req)
-    if status == "nao lido":
-        dados = {"status": "lido"}
+    if status == "lido":
+        dados = {"status": "nao lido"}
         requisicao = requests.patch(f'{link}/Imagens/{item_id}/.json', data=json.dumps(dados))
         print(image)
     
